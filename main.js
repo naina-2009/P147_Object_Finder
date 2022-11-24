34929dbbe9ac9fb88defaad99fbd325acb66e6be
naina-2009 <96481895+naina-2009@users.noreply.github.com>
2022-11-24 18:35:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+objects = [];
+status = "";
+
+function preload()
+{}
+
+function setup()
+{
+    canvas = createCanvas(500,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model is successfully Loaded!");
+    status = true;
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(video, 0,0,500,500);
+    if(status != "")
+    {
+        for(i=0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FFC300");
+            noFill();
+            stroke("#FFC300");
+            text(objects[i].label + " " + objects[i].confidence * 100 + objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x + 15, objects[i].y + 15, objects[i].width, objects[i].height)
+        }
+    }
+}
+
+
